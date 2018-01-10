aac4cacbe230ae7dc7144d199c01ed995e569e25
Bradley Whitlock <bmwhitlo@edu.uwaterloo.ca>
2018-01-09 20:46:30 -0500

    Implement using pandas

--- src/ranker.py
+++ src/ranker.py
@@ -3,6 +3,7 @@
 import argparse
 import csv
 import logging
+import pandas as pd
 
 class BuddyRanker():
 	def __init__(self, args):
@@ -14,21 +15,43 @@ class BuddyRanker():
 		console.setLevel('DEBUG' if args.debug else 'INFO')
 		self.logger.addHandler(console)
 
+		self.source_keys = ['Player1', 'Player2', 'Score1', 'Score2']
+
 
 	def read_file(self):
-		with open(self.file, 'rb') as csvfile:
-			reader = csv.reader(csvfile, delimiter=',')
-			for row in reader:
-				if len(row) != 4:
-					self.logger.critical('FATAL: CSV should contain rows with 4 columns only')
-					exit(1)
-				yield(row)
+		lst_dict = []
+		
+		self.logger.info('Reading from CSV: %s', self.file)	
+		df = pd.read_csv(self.file, delimiter=',', names=self.source_keys, header=0, error_bad_lines=False)
+		self.logger.debug('Successfully read CSV')
+		return df
+				
 
-	def generate_rank(self):
-		for game in self.read_file():
-			print game
+	def generate_game_stats(self):
+		df_data = self.read_file()
 
+		df_win_p1 = df_data[df_data.Score1 > df_data.Score2].groupby(['Player1', 'Player2']).size().reset_index(name='count').rename(columns={'Player1':'Winner','Player2':'Loser'})
+		df_win_p2 = df_data[df_data.Score1 < df_data.Score2].groupby(['Player1', 'Player2']).size().reset_index(name='count').rename(columns={'Player1':'Loser','Player2':'Winner'})
+		
+		df = df_win_p1.append(df_win_p2, ignore_index=True).groupby(['Winner', 'Loser']).sum().reset_index()
 
+		self.logger.debug('Winners results: \n %s', df)
+		self.logger.info('Gathered data on players records')
+		return df
+
+	def train_ranking(self):
+		df = self.generate_game_stats()
+
+		df_wins = df.groupby(['Winner']).sum(axis=1).drop('Loser', 1).reset_index()
+		
+		players = list(set(df['Winner']) | set(df['Loser']))
+		
+		
+		for player in players:
+
+			total_wins = df_wins[df_wins['Winner'] == row['Winner']][['count']].iloc[0]
+			
+			exit(0)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -39,6 +62,6 @@ if __name__ == '__main__':
 
 	ranker = BuddyRanker(args)
 
-	ranker.generate_rank()
+	ranker.train_ranking()
 
 
